9be8bdb6f3315e05d8e7cd77176c06982e8c4fd0
nodashin <75867423+nodashin6@users.noreply.github.com>
2022-09-02 07:25:33 +0900

    modified

--- lib/cyan/multiset.py
+++ lib/cyan/multiset.py
@@ -1,17 +1,23 @@
 import bisect
-
-class SortedSet(list):
-    def insort(self, v): bisect.insort(self, v)
-    def bisect_left(self, v): return bisect.bisect_left(self, v)
-    def bisect_right(self, v): return bisect.bisect_right(self, r)
-
 class SortedMultiset():
     """
+
     Processing Time:
     ----------------
     n       time
     2*10^5  0.183 sec  (insert only, max_capacity=4096)
     """
+
+    # ---------------------------------------------------
+    # for debug
+    # ---------------------------------------------------
+    @classmethod
+    def generate_sample(cls, v0, v1, n, rs=0):
+        import random
+        random.seed(rs)
+        a = [random.randint(v0, v1) for _ in range(n)]
+        return a
+    # ---------------------------------------------------
 
     def __init__(self, max_capacity=4096):
         self.a = []
@@ -21,13 +27,13 @@ class SortedMultiset():
         i = self._bisect_row_index(v)
         if i == -1:
             if (not self.a) or len(self.a[0]) >= self.MC:
-                self.a.insert(0, SortedSet([v]))
+                self.a.insert(0, [v])
                 return
         i = max(0, i)
-        self.a[i].insort(v)
+        bisect.insort(self.a[i], v)
         if len(self.a[i]) > self.MC:
             m = self.MC//2
-            self.a.insert(i+1, SortedSet(self.a[i][m:]))
+            self.a.insert(i+1, self.a[i][m:])
             del self.a[i][m:]
 
     def _bisect_row_index(self, v):
@@ -40,3 +46,24 @@ class SortedMultiset():
                 else:
                     r = m
         return l
+
+    def lower_bound(self, v):
+        index = 0
+        for i in range(len(self.a)):
+            if self.a[i][0] > v:
+                i -= 1
+                break
+            else:
+                index += len(self.a[i])
+        index -= len(self.a[i])
+        index += bisect.bisect_left(self.a[i], v)
+        return index
+
+    def __getitem__(self, index):
+        inner_index = 0
+        for i in range(len(self.a)):
+            if index < inner_index + len(self.a[i]):
+                return self.a[i][index-inner_index]
+            else:
+                inner_index += len(self.a[i])
+        return None
