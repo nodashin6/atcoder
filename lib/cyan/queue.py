0b0dfa4ff76108bbd93ce9811e8df1b51e7b8301
nodashin <75867423+nodashin6@users.noreply.github.com>
2022-09-06 08:04:29 +0900

    modified priorityqueue.
    processing time of ABC267E
    BEFORE: ~3.0 sec https://atcoder.jp/contests/abc267/submissions/34589364
    AFTER : ~1.1 sec https://atcoder.jp/contests/abc267/submissions/34644562

--- lib/cyan/queue.py
+++ lib/cyan/queue.py
@@ -1,104 +1,131 @@
 class VariablePriorityQueue():
+    """
+    The elements in heapq can be rewritten.
 
-    def __init__(self, n, arr=None):
+    Methods
+    -------
+    push(v, index)
+        push the element.
+    pop()
+        pop an element.
+    pushpop(v, index)
+        push the element, then pop an element.
+
+    Problems
+    --------
+    ABC267E: https://atcoder.jp/contests/abc267/submissions/34644562
+
+    Notes
+    -----
+    [1] see about heapq algorithm
+        https://github.com/python/cpython/blob/3.10/Lib/heapq.py
+    """
+
+    def __init__(self, n, a=None, INF=1<<62, has_already_heapified=False):
+        """
+        a : list[list[int, int]]
+            a must be following 2-D list. 
+            ----------------------------
+            a = [[value_0, index_0],
+                 [value_1, index_1],
+                 ...,
+                 [value_-1, index_-1]]
+            -----------------------------
+        """
         self.n = n
-        self.loc = {}
-        self.unq = set([])
-        if arr:
-            self.hq = arr
-            self._build(arr)
-        else:
-            self.hq = []
-    
-    def _build(self, arr):
-        for i, (v, index) in enumerate(arr):
-            self.loc[index] = i
-            self.unq.add(index)
-        return
+        self.INF = INF
+        self.vals = [self.INF]*self.n
+        self.locs = [None]*self.n
+        self.hq = []
+        if a:
+            for i, (v, index) in enumerate(a):
+                self.vals[index] = v
+                self.locs[index] = i
+                self.hq.append(index)
+            if not has_already_heapified:
+                self.heapify()
+
+    def heapify(self):
+        for i in reversed(range(len(self)//2)):
+            self.forward(self.hq[i])
 
     def push(self, v, index):
-        if index in self.unq:
-            self.hq[self.loc[index]][0] = v
+        if self.locs[index] is None:
+            self.vals[index], self.locs[index] = v, len(self.hq)
+            self.hq.append(index)
+            self.backward(0, index)
         else:
-            self.loc[index] = len(self.hq)
-            self.unq.add(index)
-            self.hq.append([v, index])
-        self.forward(self.loc[index])
-        
+            # `index` already exsit in heapq. 
+            if self.vals[index] < v:
+                self.vals[index] = v
+                self.forward(index)
+            elif self.vals[index] > v:
+                self.vals[index] = v
+                self.backward(0, index)
 
-    def forward(self, i, should_backward=True):
-        j = i*2+1
+    def pushpop(self, v, index):
+        """
+        Returns
+        -------
+        tuple
+            (v, index)
+        """
+        if self.locs[index] is None:
+            if self.hq and self.vals[self.hq[0]] < v:
+                self.locs[index] = 0
+                self.vals[index] = v
+                index, self.hq[0] = self.hq[0], index
+                v = self.vals[index]
+                self.forward(self.hq[0])
+            self.locs[index] = None
+            self.vals[index] = self.INF
+        else:
+            # `index` already exsit in heapq. 
+            self.push(v, index)
+            v, index = self.pop()
+        return v, index
+
+    def forward(self, index):
+        i = self.locs[index]
+        startpos = i
+        j = i*2 + 1
         while j < len(self):
-            if j+1 < len(self) and self.hq[j] > self.hq[j+1]:
+            if j+1 < len(self) and  self.vals[self.hq[j+1]] < self.vals[self.hq[j]]:
                 j += 1
-            if self.hq[i] < self.hq[j]:
-                break
-            self.hq[i], self.hq[j] = self.hq[j], self.hq[i]
-            self.loc[self.hq[i][1]] = i
-            self.loc[self.hq[j][1]] = j
+            self.hq[i], self.locs[self.hq[j]] = self.hq[j], i
             i, j = j, j*2+1
-        if should_backward:
-            self.backward(j=i)
+        self.hq[i], self.locs[index] = index, i
+        self.backward(startpos, index)
 
-    def backward(self, j):
-        while j > 0:
+    def backward(self, startpos, index):
+        j = self.locs[index]
+        while j > startpos:
             i = (j-1)//2
-            if self.hq[i] < self.hq[j]:
+            if self.vals[self.hq[i]] < self.vals[index]:
                 break
-            self.hq[i], self.hq[j] = self.hq[j], self.hq[i]
-            self.loc[self.hq[i][1]] = i
-            self.loc[self.hq[j][1]] = j
+            self.hq[j], self.locs[self.hq[i]] = self.hq[i], j
             j = i
+        self.hq[j], self.locs[index] = index, j
 
     def pop(self):
-        if not self:
-            raise IndexError("pop from empty list")
-
-        (v, i) = self.hq[0]
-        self.loc.pop(i)
-        self.unq.remove(i)
-        if len(self) == 1:
-            self.hq.pop()
-        else:
-            self.hq[0] = self.hq.pop()
-            self.loc[self.hq[0][1]] = 0
-            self.forward(i=0, should_backward=False)
-        return (v, i)
+        """
+        Returns
+        -------
+        tuple
+            (v, index)
+        """
+        index = self.hq.pop()
+        self.locs[index] = 0
+        if self.hq:
+            self.hq[0], index = index, self.hq[0]
+            self.forward(self.hq[0])
+        self.locs[index] = None
+        v, self.vals[index] = self.vals[index], self.INF
+        return (v, index)
 
     def __len__(self):
         return len(self.hq)
 
-    # ---------------------------------------
-    # for debug
-    def dprint(self, *args, **kwargs):
-        if self.is_debug:
-            print(*args, **kwargs)
-
-
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-G = [[] for _ in range(N)]
-C = [0]*N
-for _ in range(M):
-    u, v = map(int, input().split())
-    u -= 1
-    v -= 1
-    G[u].append(v)
-    G[v].append(u)
-    C[u] += A[v]
-    C[v] += A[u]
-    
-arr = sorted([[ci, i] for i, ci in enumerate(C)])
-vpq = VariablePriorityQueue(n=N, arr=arr)
-not_seen = set(range(N))
-ans = 0
-for _ in range(N):
-    cx, x = vpq.pop()
-    not_seen.remove(x)
-    ans = max(ans, cx)
- 
-    for y in G[x]:
-        C[y] -= A[x]
-        if y in not_seen:
-            vpq.push(*[C[y], y])
-print(ans)
+    def __getitem__(self, key):
+        index = self.hq[0]
+        return self.vals[index], index
