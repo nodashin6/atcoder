29f9a4ebf20672501237ae29fb0cfeec6dfe5b3c
nodashin <75867423+nodashin6@users.noreply.github.com>
2023-04-01 12:09:13 +0900

    made docs with sphinx.

---
+++ docs/make.bat
@@ -0,0 +1,44 @@
+@ECHO OFF
+
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+set SOURCEDIR=source
+set BUILDDIR=../html
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
+	echo.If you don't have Sphinx installed, grab it from
+	echo.https://www.sphinx-doc.org/
+	exit /b 1
+)
+
+
+set YMD=%date:~0,4%%date:~5,2%%date:~8,2%
+set time2=%time: =0%
+set HMD=%time2:~0,2%%time2:~3,2%%time2:~6,2%
+
+:html
+%SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIR%"/"%YMD%"_"%HMD% %SPHINXOPTS% %O%
+goto end
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:end
+popd
---
+++ docs/source/index.rst
@@ -0,0 +1,26 @@
+.. nodashin.com documentation master file, created by
+   sphinx-quickstart on Sat Apr  1 10:46:28 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+############
+nodashin.com
+############
+
+*******
+Profile
+*******
+
+- atcoder: `nodashin <https://atcoder.jp/users/nodashin>`__
+- twitter: `nodashin_jpn <https://twitter.com/nodashin_jpn>`__
+- github: `nodashin6 <https://github.com/nodashin6>`__
+
+
+*****
+INDEX
+*****
+.. toctree::
+   :maxdepth: 4
+
+   edutorial/index.rst
+   library/index.rst
